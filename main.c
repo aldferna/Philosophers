3a8d6db5f334c460d63ba4119c2cd80e4c0c5cb5
adriana fernández <adrianafernandez@MacBook-Pro-de-adriana-2.local>
2025-04-15 11:30:37 +0200

    intentos

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: adrianafernandez <adrianafernandez@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/06 18:18:12 by adrianafern       #+#    #+#             */
-/*   Updated: 2025/04/14 21:31:53 by adrianafern      ###   ########.fr       */
+/*   Updated: 2025/04/15 11:29:53 by adrianafern      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,8 +38,8 @@ void precise_sleep(long time_in_ms, t_data *info) //puede fallar si la espera es
     long waiting_started;
 
 	waiting_started = get_time(info);
-    while (get_time(info) - waiting_started < time_in_ms)
-        usleep(100);
+    while (get_time(info) - waiting_started < time_in_ms) //y si time_in_ms es menor
+        usleep(50);
 }
 
 void clean_resources(t_data *info)
@@ -67,14 +67,15 @@ void *philo_death(void *arg)
 
 	info = (t_data *)arg;
 	i = 0;
-	while(1)
+	while(1) //!info->someone_died (otro !info->someone_died && i < info->num_philos)
 	{
+		//usleep(500);
 		pthread_mutex_lock(&info->meals_death_mut);
-/* 		if (info->someone_died)
+		if (info->someone_died)
 		{
 			pthread_mutex_unlock(&info->meals_death_mut);
 			return NULL;
-		} */
+		}
 		if (get_time(info) - info->philos[i].last_meal_time >= info->time_die) //+ tiempo sin comer que time_die
 		{
 			info->someone_died = 1;
@@ -91,11 +92,24 @@ void *philo_death(void *arg)
 	return NULL;
 }
 
+int check_if_dead(t_data *info)
+{
+	int death;
+
+	pthread_mutex_lock(&info->meals_death_mut);
+	death = info->someone_died;
+	pthread_mutex_unlock(&info->meals_death_mut);
+	return (death);
+}
+
 void	*philo_life(void *arg)
 {
 	t_philo *philo;
 	
 	philo = (t_philo *)arg;
+/* 	pthread_mutex_lock(&philo->info->meals_death_mut);
+	philo->last_meal_time = get_time(philo->info);
+	pthread_mutex_unlock(&philo->info->meals_death_mut); */
 	while (1)
 	{
 		pthread_mutex_lock(&philo->info->meals_death_mut);
@@ -105,12 +119,16 @@ void	*philo_life(void *arg)
             break;
 		}
 		pthread_mutex_unlock(&philo->info->meals_death_mut);
+		
 		print_message(philo, "is thinking");
 		if (philo->id % 2 == 0)
 			precise_sleep(philo->info->time_eat, philo->info);
 		//usleep(philo->id * 100);
+		
 		if (philo->id % 2 == 0)
 		{
+			if (check_if_dead(philo->info))
+				break;
 			pthread_mutex_lock(philo->fork1); //esto que pasa si esta ocupado por otro? espera
 			print_message(philo, "has taken a fork");
 			pthread_mutex_lock(philo->fork2);
@@ -118,11 +136,20 @@ void	*philo_life(void *arg)
 		}
 		else
 		{
+			if (check_if_dead(philo->info))
+				break;
+			//precise_sleep(philo->info->time_eat, philo->info);
 			pthread_mutex_lock(philo->fork2);
 			print_message(philo, "has taken a fork");
 			pthread_mutex_lock(philo->fork1);
 			print_message(philo, "has taken a fork");
 		}
+		
+/* 		pthread_mutex_lock(philo->fork1);
+        print_message(philo, "has taken a fork");
+        pthread_mutex_lock(philo->fork2);
+        print_message(philo, "has taken a fork"); */
+		
 		pthread_mutex_lock(&philo->info->meals_death_mut);
 		philo->last_meal_time = get_time(philo->info);
 		philo->meals_eaten++;
@@ -148,7 +175,7 @@ void init_threads_join(t_data *info)
 	i = 0;
 	while (i < info->num_philos)
 	{
-		pthread_mutex_lock(&info->meals_death_mut);
+		pthread_mutex_lock(&info->meals_death_mut);  //va a philo_life?
         info->philos[i].last_meal_time = get_time(info);
         pthread_mutex_unlock(&info->meals_death_mut);
 		pthread_create(&info->philos[i].thread, NULL, philo_life, &info->philos[i]); //ahí sí va &thread porque estás llenando la variable, pthread_join(...) usa el valor
